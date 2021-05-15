6bfa24c92cc328a44817c80be362a00fb7c5acd6
i-am-subhu <dassubhasish96@outlook.com>
2021-05-16 01:42:43 +0530

    modified url links

--- src/components/Body/body.component.js
+++ src/components/Body/body.component.js
@@ -6,23 +6,23 @@ function Body(props) {
         <Fragment>
            <Form>
                   <FormGroup action>
-                  <Button href = "https://t.me/khurda_vaccine_alerts" color="primary" size="lg" block>Khorda 18+</Button>
+                  <Button href = "https://telegram.me/khurda_vaccine_alerts" color="primary" size="lg" block>Khorda 18+</Button>
                    </FormGroup>
                    <FormGroup>
-                   <Button href = "https://t.me/khurda_vaccine_alerts_45" color="primary" size="lg" block>Khorda 45+</Button>
+                   <Button href = "https://telegram.me/khurda_vaccine_alerts_45" color="primary" size="lg" block>Khorda 45+</Button>
                    </FormGroup>
                    <FormGroup>
-                   <Button href = "https://t.me/cuttack_vaccine_alerts" color="primary" size="lg" block>Cuttack 18+</Button>
+                   <Button href = "https://telegram.me/cuttack_vaccine_alerts" color="primary" size="lg" block>Cuttack 18+</Button>
                    </FormGroup>
 
                    <FormGroup>
-                   <Button href = "https://t.me/cuttack_vaccine_alerts_45" color="primary" size="lg" block>Cuttack 45+</Button>
+                   <Button href = "https://telegram.me/cuttack_vaccine_alerts_45" color="primary" size="lg" block>Cuttack 45+</Button>
                    </FormGroup>
                    <FormGroup>
-                   <Button href = "https://t.me/sundargarh_vaccine_alerts" color="primary" size="lg" block>Sundargarh 18+</Button>
+                   <Button href = "https://telegram.me/sundargarh_vaccine_alerts" color="primary" size="lg" block>Sundargarh 18+</Button>
                    </FormGroup>
                    <FormGroup>
-                   <Button href = "https://t.me/sundargarh_vaccine_alerts_45" color="primary" size="lg" block>Sundargarh 45+</Button>
+                   <Button href = "https://telegram.me/sundargarh_vaccine_alerts_45" color="primary" size="lg" block>Sundargarh 45+</Button>
                    </FormGroup>
            </Form>
         </Fragment>
