8f5337468446b42190d37e5a7b97d7ec04c5f139
i-am-subhu <dassubhasish96@outlook.com>
2021-05-16 02:47:18 +0530

    updated css

--- src/components/Body/howtouse.component.js
+++ src/components/Body/howtouse.component.js
@@ -3,16 +3,16 @@ import React, { Fragment } from 'react';
 function HowToUse(props) {
     return (
         <Fragment>
-            <h2><u>F.A.Q</u></h2>
-            <h5><b>About these telegram channels ?</b></h5>
+            <h4><u>F.A.Q</u></h4>
+            <h4><i>About these telegram channels ?</i></h4>
              There are different telegram channels for each district. Whenever vaccine slots become available for the district, availability
               details will be sent in the channel immediately.
-            <h5><b>How to join telegram channels ?</b></h5>
+            <h4><i>How to join telegram channels ?</i></h4>
              Click on the channel joining link of your district. Open in telegram app. Click on join button shown below<br/>
-             <h5><b>How is these telegram channels working ?</b></h5>
+             <h4><i>How is these telegram channels working ?</i></h4>
             There are some bots running in the background which checks slots availability of district through public cowin apis continuosly and sends
              notification in the channel.
-             <h5><b>Any Query ?</b></h5>
+             <h4><i>Any Query ?</i></h4>
             For any queries or discussion related to these channels, please join below telegram group.<br/> <a href = "https://t.me/odisha_vaccine_alerts">https://t.me/odisha_vaccine_alerts</a><br/>
 
            
--- src/components/shared/Header/header.component.js
+++ src/components/shared/Header/header.component.js
@@ -9,7 +9,7 @@ function Header(props) {
     return (
         <Fragment>
              <img class = "image" src= {banner} alt="vaccine banner" ></img>
-           <h2>District wise telegram channels in Odisha</h2>
+           <h3>District wise telegram channels in Odisha</h3>
            <p>Following are telegram channels for all the districts in odisha. Other districts will be added soon.</p>
         </Fragment>
     );
