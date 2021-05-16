27e1108e695a3082dd90966985a8008c5a72c733
i-am-subhu <dassubhasish96@outlook.com>
2021-05-16 15:59:35 +0530

    updated js

--- src/components/Body/howtouse.component.js
+++ src/components/Body/howtouse.component.js
@@ -8,12 +8,12 @@ function HowToUse(props) {
              There are different telegram channels for each district. Whenever vaccine slots become available for the district, availability
               details will be sent in the channel immediately.
             <h4><i>How to join telegram channels ?</i></h4>
-             Click on the channel joining link of your district. Open in telegram app. Click on join button shown below<br/>
+             Click on the channel joining link of your district. Open in telegram app. Click on join button shown below.<br/>
              <h4><i>How is these telegram channels working ?</i></h4>
             There are some bots running in the background which checks slots availability of district through public cowin apis continuosly and sends
              notification in the channel.
              <h4><i>Any Query ?</i></h4>
-            For any queries or discussion related to these channels, please join below telegram group.<br/> <a href = "https://t.me/odisha_vaccine_alerts">https://t.me/odisha_vaccine_alerts</a><br/>
+            For any queries or discussion related to these channels, please join below telegram group.<br/> <a href = "https://telegram.me/odisha_vaccine_alerts">https://t.me/odisha_vaccine_alerts</a><br/>
 
            
         </Fragment>
