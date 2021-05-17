6b207040d70e094036f12e2ec2978f0f47ff19a2
i-am-subhasish <dassubhasish96@gmail.com>
2021-05-17 16:15:53 +0530

    added sambalpur

--- src/components/Body/body.component.js
+++ src/components/Body/body.component.js
@@ -24,6 +24,12 @@ function Body(props) {
                    <FormGroup>
                    <Button href = "https://telegram.me/sundargarh_vaccine_alerts_45" color="primary" size="lg" block>Sundargarh (Age 45+)</Button>
                    </FormGroup>
+                   <FormGroup>
+                   <Button href = "https://telegram.me/sambalpur_vaccine_alerts" color="primary" size="lg" block>Sambalpur (Age 18 - 44)</Button>
+                   </FormGroup>
+                   <FormGroup>
+                   <Button href = "https://telegram.me/sambalpur_vaccine_alerts_45" color="primary" size="lg" block>Sambalpur (Age 45+)</Button>
+                   </FormGroup>
            </Form>
         </Fragment>
     );
