c21f2543e3ef243763d4ebdd371429add23a2ed5
i-am-subhasish <dassubhasish96@gmail.com>
2021-05-18 19:56:35 +0530

    changed ui

--- src/components/Body/body.component.js
+++ src/components/Body/body.component.js
@@ -6,47 +6,37 @@ function Body(props) {
         <Fragment>
            <Form>
                   <FormGroup action>
-                  <Button href = "https://telegram.me/khurda_vaccine_alerts" color="primary" size="lg" block>Khorda (Age 18 - 44)</Button>
+                  <Button href = "https://telegram.me/khurda_vaccine_alerts" color="primary" size="lg" block>Khorda</Button>
                    </FormGroup>
+    
                    <FormGroup>
-                   <Button href = "https://telegram.me/khurda_vaccine_alerts_45" color="primary" size="lg" block>Khorda (Age 45+)</Button>
-                   </FormGroup>
-                   <FormGroup>
-                   <Button href = "https://telegram.me/cuttack_vaccine_alerts" color="primary" size="lg" block>Cuttack (Age 18 - 44)</Button>
+                   <Button href = "https://telegram.me/cuttack_vaccine_alerts" color="primary" size="lg" block>Cuttack</Button>
                    </FormGroup>
 
+ 
                    <FormGroup>
-                   <Button href = "https://telegram.me/cuttack_vaccine_alerts_45" color="primary" size="lg" block>Cuttack (Age 45+)</Button>
+                   <Button href = "https://telegram.me/sundargarh_vaccine_alerts" color="primary" size="lg" block>Sundargarh</Button>
+                   </FormGroup>
+                  
+                   <FormGroup>
+                   <Button href = "https://telegram.me/sambalpur_vaccine_alerts" color="primary" size="lg" block>Sambalpur</Button>
+                   </FormGroup>
+ 
+                   <FormGroup>
+                   <Button href = "https://telegram.me/balasore_vaccine_alerts" color="primary" size="lg" block>Balasore</Button>
+                   </FormGroup>
+                  
+                   <FormGroup>
+                   <Button href = "https://telegram.me/angul_vaccine_alert" color="primary" size="lg" block>Angul</Button>
                    </FormGroup>
                    <FormGroup>
-                   <Button href = "https://telegram.me/sundargarh_vaccine_alerts" color="primary" size="lg" block>Sundargarh (Age 18 - 44)</Button>
+                   <Button href = "https://telegram.me/jajpur_vaccine_alerts" color="primary" size="lg" block>Jajpur</Button>
                    </FormGroup>
                    <FormGroup>
-                   <Button href = "https://telegram.me/sundargarh_vaccine_alerts_45" color="primary" size="lg" block>Sundargarh (Age 45+)</Button>
+                   <Button href = "https://telegram.me/puri_vaccine_alerts" color="primary" size="lg" block>Puri</Button>
                    </FormGroup>
                    <FormGroup>
-                   <Button href = "https://telegram.me/sambalpur_vaccine_alerts" color="primary" size="lg" block>Sambalpur (Age 18 - 44)</Button>
-                   </FormGroup>
-                   <FormGroup>
-                   <Button href = "https://telegram.me/sambalpur_vaccine_alerts_45" color="primary" size="lg" block>Sambalpur (Age 45+)</Button>
-                   </FormGroup>
-                   <FormGroup>
-                   <Button href = "https://telegram.me/balasore_vaccine_alerts" color="primary" size="lg" block>Balasore (Age 18 - 44)</Button>
-                   </FormGroup>
-                   <FormGroup>
-                   <Button href = "https://telegram.me/balasore_vaccine_alerts_45" color="primary" size="lg" block>Balasore (Age 45+)</Button>
-                   </FormGroup>
-                   <FormGroup>
-                   <Button href = "https://telegram.me/angul_vaccine_alert" color="primary" size="lg" block>Angul (All Age)</Button>
-                   </FormGroup>
-                   <FormGroup>
-                   <Button href = "https://telegram.me/jajpur_vaccine_alerts" color="primary" size="lg" block>Jajpur (All Age)</Button>
-                   </FormGroup>
-                   <FormGroup>
-                   <Button href = "https://telegram.me/puri_vaccine_alerts" color="primary" size="lg" block>Puri (All Age)</Button>
-                   </FormGroup>
-                   <FormGroup>
-                   <Button href = "https://telegram.me/bargarh_vaccine_alerts" color="primary" size="lg" block>Bargarh (All Age)</Button>
+                   <Button href = "https://telegram.me/bargarh_vaccine_alerts" color="primary" size="lg" block>Bargarh</Button>
                    </FormGroup>
            </Form>
         </Fragment>
